5327cc8a4164dec6af17ea515527cff221323922
Maks Kostyshen <maxxxymko2003@gmail.com>
2023-01-12 13:04:15 +0300

    Initial commit

---
+++ src/main/java/com/example/owlmail/OwlMailApplication.java
@@ -0,0 +1,16 @@
+package com.example.owlmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application class.
+ */
+@SpringBootApplication
+public class OwlMailApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OwlMailApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/owlmail/OwlMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.owlmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlMailApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
