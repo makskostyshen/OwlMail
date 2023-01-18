d7f906c256267813a955e1309af8aed43948f4ca
Maks Kostyshen <maxxxymko2003@gmail.com>
2023-01-18 17:50:09 +0300

    :sparkles: Update schema.sql and data.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,12 +12,29 @@ CREATE TABLE wizard (
 
 CREATE TABLE owl (
     id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255),
     breed VARCHAR(255),
     date_of_birth DATE,
-    name VARCHAR(255),
     ownership_start_date DATE,
     wizard_family_name VARCHAR(255),
     wizard_first_name VARCHAR(255),
     FOREIGN KEY (wizard_family_name, wizard_first_name)
+        REFERENCES wizard (family_name, first_name)
+);
+
+CREATE TABLE letter(
+    id BIGSERIAL PRIMARY KEY,
+    header VARCHAR(255),
+    body VARCHAR(255),
+    date_time TIMESTAMP,
+    sender_family_name VARCHAR(255),
+    sender_first_name VARCHAR(255),
+    owl_id INT,
+    receiver_family_name VARCHAR(255),
+    receiver_first_name VARCHAR(255),
+    FOREIGN KEY (sender_family_name, sender_first_name)
+        REFERENCES wizard (family_name, first_name),
+    FOREIGN KEY (owl_id) REFERENCES owl (id),
+    FOREIGN KEY (receiver_family_name, receiver_first_name)
         REFERENCES wizard (family_name, first_name)
 );
